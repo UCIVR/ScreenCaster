d38eae192fea85fd935076b87f8c780fbf0ca844
David Detweiler <daviddetweiler1@gmail.com>
2023-05-04 05:40:56 -0700

    Live textures!

--- Source/ScreenCaster/Private/ScreenCasterComponent.cpp
+++ Source/ScreenCaster/Private/ScreenCasterComponent.cpp
@@ -18,6 +18,8 @@ UScreenCasterComponent::UScreenCasterComponent()
 void UScreenCasterComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	texture->Initialize(1920, 1080, FLinearColor::Blue);
+	texture->UpdateTexture();
 
 	// ...
 }
@@ -32,17 +34,41 @@ void UScreenCasterComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 
 bool UScreenCasterComponent::Connect()
 {
-	conductor::log_function* logger = [](bool is_error, const wchar_t* str) {
+	conductor::log_function* logger = [](void* self, bool is_error, const wchar_t* str) {
 		if (is_error) {
 			UE_LOG(LogTemp, Error, TEXT("%s"), str);
 		}
 		else {
-			UE_LOG(LogTemp, Error, TEXT("%s"), str);
+			UE_LOG(LogTemp, Warning, TEXT("%s"), str);
 		}
 	};
 
 	UE_LOG(LogTemp, Log, TEXT("Connecting..."));
-	observer.emplace(logger);
+	OnTestEvent.Broadcast();
+	observer.emplace(this, logger, [](void* self, auto... others) {reinterpret_cast<UScreenCasterComponent*>(self)->on_video_frame(others...); });
 	observer->start();
 	return true;
+}
+
+void UScreenCasterComponent::on_video_frame(std::uint64_t width, std::uint64_t height, conductor::video_callback* callback, void* data)
+{
+	{
+		std::lock_guard guard{ texture_lock };
+		if (!callback(data, texture->GetBuffer(), texture->GetBufferSize())) {
+			UE_LOG(LogTemp, Error, TEXT("Buffer fill failure"));
+		}
+	}
+
+	AsyncTask(
+		ENamedThreads::GameThread,
+		[=] {
+			std::lock_guard guard{ texture_lock };
+			texture->UpdateTexture();
+		}
+	);
+}
+
+UDynamicTexture* UScreenCasterComponent::GetTexture()
+{
+	return texture;
 }
--- Source/ScreenCaster/Public/ScreenCasterComponent.h
+++ Source/ScreenCaster/Public/ScreenCasterComponent.h
@@ -5,10 +5,15 @@
 #include "../../../../webrtc-native/src/examples/libconductor/conductor.h"
 
 #include <optional>
+#include <mutex>
+
+#include "DynamicTexture.h"
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "ScreenCasterComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTestEvent);
 
 UCLASS(BlueprintType, Blueprintable, Category = "ScreenCaster", META = (DisplayName = "ScreenCaster Component", BlueprintSpawnableComponent))
 class SCREENCASTER_API UScreenCasterComponent : public UActorComponent
@@ -25,11 +30,22 @@ protected:
 
 public:
 	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintCallable, Category = "ScreenCaster", META = (DisplayName = "Connect"))
-	bool Connect();
+		bool Connect();
+
+	UFUNCTION(BlueprintCallable, Category = "ScreenCaster", META = (DisplayName = "GetTexture"))
+		UDynamicTexture* GetTexture();
+
+	UDynamicTexture* texture{ NewObject<UDynamicTexture>() };
+
+	UPROPERTY(BlueprintAssignable)
+		FTestEvent OnTestEvent;
 
 private:
 	std::optional<conductor::observer_handle> observer{};
+	std::mutex texture_lock{};
+
+	void on_video_frame(std::uint64_t width, std::uint64_t height, conductor::video_callback* callback, void* data);
 };
